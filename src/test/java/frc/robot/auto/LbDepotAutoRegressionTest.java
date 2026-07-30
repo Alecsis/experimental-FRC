9be5f8782dc214a24e5cf820845cd123ee3acb0a
alecsis <josephddang00@gmail.com>
2026-07-29 23:41:34 -0400

    auto: expand regression suite to all routes

---
+++ src/test/java/frc/robot/auto/LbDepotAutoRegressionTest.java
@@ -0,0 +1,6 @@
+package frc.robot.auto;
+
+class LbDepotAutoRegressionTest extends AutoRegressionTestBase {
+    @Override protected String autoName() { return "LB Depot"; }
+    @Override protected String goldenFileName() { return "LB_Depot.json"; }
+}
