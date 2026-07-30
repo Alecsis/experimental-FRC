5f9091855d7e9688e8032e53248ca3b9bbc58f83
alecsis <josephddang00@gmail.com>
2026-07-29 23:30:47 -0400

    auto: baseline LB and RB neutral routes

---
+++ src/test/java/frc/robot/auto/LbNeutralAutoRegressionTest.java
@@ -0,0 +1,13 @@
+package frc.robot.auto;
+
+class LbNeutralAutoRegressionTest extends AutoRegressionTestBase {
+    @Override
+    protected String autoName() {
+        return "LB Neutral";
+    }
+
+    @Override
+    protected String goldenFileName() {
+        return "LB_Neutral.json";
+    }
+}
---
+++ src/test/java/frc/robot/auto/RbNeutralAutoRegressionTest.java
@@ -0,0 +1,13 @@
+package frc.robot.auto;
+
+class RbNeutralAutoRegressionTest extends AutoRegressionTestBase {
+    @Override
+    protected String autoName() {
+        return "RB Neutral";
+    }
+
+    @Override
+    protected String goldenFileName() {
+        return "RB_Neutral.json";
+    }
+}
