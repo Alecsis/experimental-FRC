74f9891e2eb1749bd0c634ee07bcdb58c4392230
alecsis <josephddang00@gmail.com>
2026-07-30 20:59:50 -0400

    review fixes: golden msg direction, static close note, vault path back, test style

--- src/test/java/frc/robot/auto/RbNeutralAutoRegressionTest.java
+++ src/test/java/frc/robot/auto/RbNeutralAutoRegressionTest.java
@@ -1,13 +1,6 @@
 package frc.robot.auto;
 
 class RbNeutralAutoRegressionTest extends AutoRegressionTestBase {
-    @Override
-    protected String autoName() {
-        return "RB Neutral";
-    }
-
-    @Override
-    protected String goldenFileName() {
-        return "RB_Neutral.json";
-    }
+    @Override protected String autoName() { return "RB Neutral"; }
+    @Override protected String goldenFileName() { return "RB_Neutral.json"; }
 }
